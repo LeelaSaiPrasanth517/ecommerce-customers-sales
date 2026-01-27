716af4e1ef20dd36f4d4107f4d4183de6c160495
LeelaSaiPrasanth517 <leelasaiprasanth61@gmail.com>
2026-01-27 21:03:22 +0530

    Add SQL queries for customer behavior analysis

---
+++ customer_analysis.sql
@@ -0,0 +1,33 @@
+--Total Unique Customers
+
+SELECT 
+    COUNT(DISTINCT customerid) AS unique_customers
+FROM transactions;
+
+--Top 10 Customers by Spend
+
+SELECT customerid, sum(quantity*price*(1-discount)) as Money_spent
+ FROM transactions
+GROUP BY customerid
+ORDER BY Money_spent DESC
+LIMIT 10
+
+--Average spend across all customers
+
+SELECT avg(Total_spend) as Average_spend FROM
+(SELECT customerid, sum(quantity*price*(1-discount)) as Total_spend
+FROM transactions
+GROUP BY customerid)t;
+
+--Repeat vs One-Time Customers
+
+SELECT customerid,
+    CASE 
+        when order_count = 1 then 'One time'
+        else 'Repeat'
+    END as customer_type 
+    FROM
+(SELECT customerid,count(customerid) as order_count
+FROM transactions 
+GROUP BY customerid) t
+
